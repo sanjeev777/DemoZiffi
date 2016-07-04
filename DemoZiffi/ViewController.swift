bb51c3e1751c86ab0ef24d0627f62f6d45597758
Sanjeev Suman <sanjeevsuman.777@gmail.com>
2016-07-04 18:37:05 +0530

    Initial commit - Added on boarding images

---
+++ DemoZiffi/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  DemoZiffi
+//
+//  Created by ziffi on 7/1/16.
+//  Copyright © 2016 ziffi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    var imageView: UIImageView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        for i in 0..<4
+        {
+            let imageView = UIImageView(image: UIImage(named: "Image-\(i+1)"))
+            imageView.frame.size = scrollView.frame.size
+            imageView.frame.origin.y = 0
+            imageView.frame.origin.x = CGFloat(i) * scrollView.frame.width
+            scrollView.addSubview(imageView)
+        }
+        scrollView.contentSize.width = 4 * scrollView.frame.width
+    }
+    
+    func showLoginScreen()
+    {
+        if let lg = storyboard?.instantiateViewControllerWithIdentifier("LoginPage") as? LoginViewController
+        {
+            navigationController?.pushViewController(lg, animated: true)
+        }
+    }
+
+    @IBAction func login(sender: UIButton) {
+        showLoginScreen()
+    }
+    
+    
+    @IBAction func signup(sender: UIButton) {
+    }
+    
+
+}
+
---
+++ LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  DemoZiffi
+//
+//  Created by ziffi on 7/1/16.
+//  Copyright © 2016 ziffi. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    //@IBOutlet weak var fbButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginFacebook(sender: UIButton) {
+    }
+    @IBAction func logingoogle(sender: UIButton) {
+    }
+    
+    @IBAction func login(sender: UIButton) {
+    }
+}
